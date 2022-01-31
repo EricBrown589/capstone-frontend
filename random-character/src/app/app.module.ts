4c70d42efc7519322831a7b45a58535874052caa
Eric Brown <ericbrown589@yahoo.com>
2022-01-31 10:53:40 -0600

    add components for header, character, and home

--- random-character/src/app/app.module.ts
+++ random-character/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CharacterComponent } from './character/character.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CharacterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
