6f16e6366046e7b5684d1f395f0ca09c2119bab5
Eric Brown <ericbrown589@yahoo.com>
2022-02-03 11:54:57 -0600

    add url to environments

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Character } from './character';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  apiUrl = 'https://random-character-backend.herokuapp.com/'
+  apiUrl = environment.url
 
   constructor(private http: HttpClient) { }
 
