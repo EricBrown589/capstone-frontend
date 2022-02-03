9a465abe63648e3eb2fa4cd296aea3275f46dc8c
Eric Brown <ericbrown589@yahoo.com>
2022-02-02 23:39:02 -0600

    update deployment test

---
+++ src/app/character.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Character } from './character';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+
+  apiUrl = 'http://localhost:9092'
+
+  constructor(private http: HttpClient) { }
+
+  public getCharacter(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.apiUrl}/character`) 
+  }
+
+  public createCharacter(): Observable<any> {
+    const headers = {'content-type': 'application/json'}
+    const body = {}
+    return this.http.post<Character>(`${this.apiUrl}/create-character`, body, {'headers': headers})
+  }
+
+  public deleteCharacter(characterId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete-character/${characterId}`)
+  }
+}
